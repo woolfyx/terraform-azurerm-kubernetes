bd7a40f35687220fffbacde509471c54157fdfb8
woolfyx <29287203+woolfyx@users.noreply.github.com>
2021-02-28 20:50:35 +0100

    Update versions

--- providers.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   required_version = ">= 0.14.0"
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = ">= 2.49.0"
-#     }
-#   }
-# }
